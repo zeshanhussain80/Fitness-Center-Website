74473d1428258b73609737735c46df6f6728efed
zeshanhussain80 <168620127+zeshanhussain80@users.noreply.github.com>
2024-05-01 11:17:42 +0500

    Added script.js

---
+++ script.js
@@ -0,0 +1,72 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".header__content h2", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header__content p", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".header__content .header__btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".about__card", {
+  duration: 1000,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".trainer__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".blog__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const links = document.querySelectorAll(".navbar a");
+
+  links.forEach(link => {
+    link.addEventListener("click", function(event) {
+      event.preventDefault(); // Prevent default link behavior
+
+      const targetId = link.getAttribute("href").substring(1); // Get target section ID
+      const targetSection = document.getElementById(targetId); // Get target section element
+
+      if (targetSection) {
+        // Scroll to the target section
+        targetSection.scrollIntoView({
+          behavior: "smooth"
+        });
+      }
+    });
+  });
+});
